414a31ad14dd95d98e905bee069db752c35e9bb3
federicopiaggio <federicopiaggio26@gmail.com>
2020-04-14 18:33:17 -0300

    filtro de imagenes funcionando

---
+++ imagenes.js
@@ -0,0 +1,62 @@
+class Image {
+    constructor(name, type, description){
+        this.name = name;
+        this.type = type;
+        this.description = description;
+    }
+
+}
+
+
+var images = [
+        new Image("img1","comic","trabajo 1"),
+        new Image("img2","comic","trabajo 2"),
+        new Image("img3","comic","trabajo 3"),
+        new Image("img4","comic","trabajo 4"),
+        new Image("img5","comic","trabajo 5"),
+        new Image("img6","comic","trabajo 6"),
+        new Image("img7","comic","trabajo 6"),
+        new Image("img8","comic","trabajo 6"),
+        new Image("img9","comic","trabajo 6"),
+        new Image("img10","Illustration","trabajo 6"),
+        new Image("img11","Illustration","trabajo 6"),
+        new Image("img12","Illustration","trabajo 6"),
+        new Image("img13","Illustration","trabajo 6"),
+        new Image("img14","Illustration","trabajo 6"),
+        new Image("img15","Illustration","trabajo 6"),
+        new Image("img16","Illustration","trabajo 6"),
+        new Image("img17","Illustration","trabajo 6"),
+        new Image("img18","Illustration","trabajo 6"),
+        new Image("img19","Illustration","trabajo 6"),
+        new Image("img20","Illustration","trabajo 6"),
+        new Image("img21","Illustration","trabajo 6"),
+        new Image("img22","Illustration","trabajo 6"),
+        new Image("img23","Illustration","trabajo 6"),
+        new Image("img24","Illustration","trabajo 6"),
+        new Image("img25","Illustration","trabajo 6"),
+        new Image("img26","Illustration","trabajo 6"),
+        new Image("img27","Illustration","trabajo 6"),
+        new Image("img28","character","trabajo 6"),
+        new Image("img29","character","trabajo 6"),
+        new Image("img30","character","trabajo 6"),
+        new Image("img31","character","trabajo 6"),
+        new Image("img32","character","trabajo 6"),
+        new Image("img33","character","trabajo 6"),
+        new Image("img34","character","trabajo 6"),
+        new Image("img35","character","trabajo 6"),
+        new Image("img36","character","trabajo 6"),
+
+
+];
+
+
+/* 
+var cartoonFiltered = images.filter( image => image.type == "cartoon");
+var advertisingFiltered = images.filter( image => image.type == "advertising"); */
+
+    
+
+
+
+
+
--- index.js
+++ index.js
@@ -37,4 +37,42 @@ $(".nav-container li").hover(function(){
             ,200);
 });
 
-$
+function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+  }
+
+shuffle(images);
+
+images.forEach(image => {
+    $('.main-gallery').append('<img' + ' class="' + image.type + '"' + 'src="./img/gallery/'+ image.name + '.jpg">');
+});
+
+
+
+
+$("#illustration").click(()=>{
+    $(".character").fadeOut();
+    $(".comic").fadeOut();
+    $(".Illustration").fadeIn();
+    
+});
+
+$("#comic").click(()=>{
+    $(".character").fadeOut();
+    $(".Illustration").fadeOut();
+    $(".comic").fadeIn();
+
+});
+
+$("#character").click(()=>{
+    $(".comic").fadeOut();
+    $(".Illustration").fadeOut();
+    $(".character").fadeIn();
+
+});
+
+$("#all").click(()=>{
+    $(".character").fadeIn();
+    $(".Illustration").fadeIn();
+    $(".comic").fadeIn();
+});
